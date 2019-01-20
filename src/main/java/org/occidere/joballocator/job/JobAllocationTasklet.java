28aeebd8e762c52244e71f1404ac67658a2fe386
occidere <occidere@naver.com>
2019-01-20 18:30:09 +0900

    parallel stream 방식 적용 & 30분 스케줄링

--- src/main/java/org/occidere/joballocator/job/JobAllocationTasklet.java
+++ src/main/java/org/occidere/joballocator/job/JobAllocationTasklet.java
@@ -14,11 +14,14 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * @author occidere
@@ -28,10 +31,17 @@ import java.util.List;
  */
 public class JobAllocationTasklet implements Tasklet, StepExecutionListener {
 	private Logger logger = LoggerFactory.getLogger(JobAllocationTasklet.class);
-	private int minuteRange = 10; // TODO JobParameters 로 변경 필요
+
+	private int minuteRange = 30; // TODO JobParameters 로 변경 필요
+
+	@Value("${job.allocator.execute.thread.count}")
+	private int jobAllocatorExecuteThreadCount;
 
 	@Autowired
 	private WebClient webClient;
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@Autowired
 	private JobInfoRepository jobInfoRepository;
@@ -42,23 +52,27 @@ public class JobAllocationTasklet implements Tasklet, StepExecutionListener {
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		LocalTime curTime = LocalTime.now();
+		LocalDateTime curTime = LocalDateTime.now();
 
 		logger.info("현재 시간: {}", curTime);
-		String from = curTime.minusMinutes(minuteRange).format(DateTimeFormatter.ofPattern("HHmm")) + "00";
-		String to = curTime.format(DateTimeFormatter.ofPattern("HHmm")) + "59";
+		String from = curTime.minusMinutes(minuteRange).toLocalTime().format(DateTimeFormatter.ofPattern("HHmm")) + "00";
+		String to = curTime.toLocalTime().format(DateTimeFormatter.ofPattern("HHmm")) + "59";
 
 		logger.info("잡 할당 체크 시간범위: {} ~ {}", from, to);
 
 		List<JobInfo> jobInfos = jobInfoRepository.findJobInfosByOnSchedule(from, to, JobStatus.SERVICE_ON.getFieldName());
 		logger.info("스케줄링 된 잡 개수: {}", jobInfos.size());
 
-		for (JobInfo jobInfo : jobInfos) {
-			String jobCallUrl = jobInfo.getJobCallUrl();
-			logger.info("호출: {}", jobCallUrl);
-			webClient.get().uri(jobCallUrl);
-		}
+		logger.info("스레드 개수: {}", jobAllocatorExecuteThreadCount);
 
+		new ForkJoinPool(jobAllocatorExecuteThreadCount).submit(() ->
+				jobInfos.parallelStream()
+						.forEach(jobInfo -> {
+							String jobCallUrl = jobInfo.getJobCallUrl();
+							logger.info("호출 Job 정보: {}", jobInfo.toString());
+//							webClient.get().uri(jobCallUrl).exchange().block();
+							restTemplate.execute(jobCallUrl, HttpMethod.GET, null, null);
+						})).get();
 
 		return RepeatStatus.FINISHED;
 	}
